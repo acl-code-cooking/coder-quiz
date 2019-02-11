cf2eec5e4b451f619f81981d26137e8b9593e897
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-02-10 18:54:14 -0800

    scroll test pass

--- src/result/calculate/score-treasure.js
+++ src/result/calculate/score-treasure.js
@@ -2,6 +2,8 @@ function scoreTreasure(answer, scorecard) {
   if(answer === 'gold') {
     scorecard.pirate += 0.5
     scorecard.viking += 0.5
+  } else if(answer === 'scroll') {
+    scorecard.ninja += 1
   }
 }
 
--- test/treasure/score-treasure.test.js
+++ test/treasure/score-treasure.test.js
@@ -13,3 +13,9 @@ test('gold', function(assert) {
   scoreTreasure('gold', scorecard);
   assert.deepEqual(scorecard, { pirate: 0.5, ninja: 0, viking: 0.5})
 });
+
+test('scroll', function(assert) {
+  scoreTreasure('scroll', scorecard);
+  assert.deepEqual(scorecard, { pirate: 0, ninja: 1, viking: 0})
+});
+
